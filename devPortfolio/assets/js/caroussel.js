476a1f362bbede7cf3b0b1c28404214d8c0d68fa
joris-Bourguet <43138838+joris-Bourguet@users.noreply.github.com>
2021-01-17 18:15:29 +0100

    sécurité sur les hash et affichages des projectInfo

--- devPortfolio/assets/js/caroussel.js
+++ devPortfolio/assets/js/caroussel.js
@@ -9,6 +9,7 @@ function caroussel() {
 
     $(projectInfo).css("top", top + "px");
 
+
     var mySwiper = new Swiper('.swiper-container', {
         // Optional parameters
         speed: 400,
@@ -44,6 +45,33 @@ function caroussel() {
         },
     })
 
+    let title = {
+        portfolio: {
+            "hash": "#Portfolio_vuejs",
+            "titre": "Portfolio",
+            "description": "Site de websitedev"
+        },
+        pole_mecanique_karting_ales: {
+            "hash": "#pole-mecanique-karting-ales",
+            "titre": "Pole Mécanique Karting Ales",
+            "description": "Le site du pole mécanique karting d'ales a été remis au goût du jours"
+        },
+        hotel_equipe_sauze: {
+            "hash": "#hotel-equipe-sauze",
+            "titre": "Hotel L'Équipe",
+            "description": "Site de L'hotel L'équipe"
+        },
+        websitedev: {
+            "hash": "#websitedev",
+            "titre": "websitedev.fr",
+            "description": "Site de websitedev"
+        }
+    }
+    if (!location.hash) {
+        location.hash = title.portfolio.hash
+    } else {
+        changeInfo(location.hash)
+    }
 
     window.addEventListener('hashchange', (e) => {
         changeInfo(location.hash)
@@ -51,16 +79,23 @@ function caroussel() {
     })
 
     function changeInfo(hash) {
-        if (hash == "#websitedev") {
-            projectTitle.textContent = "test"
+        if (hash == title.portfolio.hash) {
+            projectTitle.textContent = title.portfolio.titre
+            projectDescription.textContent = title.portfolio.description
+        } else if (hash == title.pole_mecanique_karting_ales.hash) {
+            projectTitle.textContent = title.pole_mecanique_karting_ales.titre
+            projectDescription.textContent = title.pole_mecanique_karting_ales.description
+        } else if (hash == title.hotel_equipe_sauze.hash) {
+            projectTitle.textContent = title.hotel_equipe_sauze.titre
+            projectDescription.textContent = title.hotel_equipe_sauze.description
+        } else if (hash == title.websitedev.hash) {
+            projectTitle.textContent = title.websitedev.titre
+            projectDescription.textContent = title.websitedev.description
+        } else {
+            location.hash = title.portfolio.hash
         }
     }
 
-    let title = {
-        "pole-mecanique-karting-ales": "Titre pole",
-        "hotel-equipe-sauze": "Titre hotel",
-        "websitedev": "Titre wsd",
-    }
 }
 
 export { caroussel }
