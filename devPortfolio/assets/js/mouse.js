e58341b81fd705dafecc05ab730cbd01d29d8965
joris-Bourguet <43138838+joris-Bourguet@users.noreply.github.com>
2021-02-08 11:37:53 +0100

    v2 mouse following

--- devPortfolio/assets/js/mouse.js
+++ devPortfolio/assets/js/mouse.js
@@ -2,10 +2,13 @@ function mousePos() {
     let mouseDiv = document.querySelector('.mouseCircle')
     $(document).mousemove(function(e) {
         // values: e.clientX, e.clientY, e.pageX, e.pageY
-        mouseDiv.animate({
-            top: e.pageY,
-            left: e.pageX
-        }, 1000)
+        // mouseDiv.animate({
+        //     top: e.pageY,
+        //     left: e.pageX
+        // }, 1000)
+
+        $(mouseDiv).css("top", e.pageY);
+        $(mouseDiv).css("left", e.pageX);
     })
 }
 
