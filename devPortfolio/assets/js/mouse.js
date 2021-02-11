adf05f2806290d3cd5fe7013f9b6163209f37b8b
joris-Bourguet <43138838+joris-Bourguet@users.noreply.github.com>
2021-02-11 18:43:24 +0100

    souris detect anti overflow

--- devPortfolio/assets/js/mouse.js
+++ devPortfolio/assets/js/mouse.js
@@ -1,14 +1,27 @@
 function mousePos() {
+    const taillePageX = window.innerWidth
+    const taillePageY = window.innerHeight
+
+    console.log(taillePageX, taillePageY);
+
     let mouseDiv = document.querySelector('.mouseCircle')
     $(document).mousemove(function(e) {
-        // values: e.clientX, e.clientY, e.pageX, e.pageY
-        // mouseDiv.animate({
-        //     top: e.pageY,
-        //     left: e.pageX
-        // }, 1000)
-
         $(mouseDiv).css("top", e.pageY);
         $(mouseDiv).css("left", e.pageX);
+
+        // empecher la div de dépasser du body (beta)
+        if (e.pageY < 20) {
+            $(mouseDiv).css("top", 20);
+        }
+        if (e.pageY > taillePageY - 30) {
+            $(mouseDiv).css("top", taillePageY - 30);
+        }
+        if (e.pageX < 20) {
+            $(mouseDiv).css("left", 20);
+        }
+        if (e.pageX > taillePageX - 20) {
+            $(mouseDiv).css("left", taillePageX - 20);
+        }
     })
 }
 
